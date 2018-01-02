3ec2b354d488a337fe6cca7cc4b8de494618db62
LeimingHu <huleiming1991@gmail.com>
2018-01-02 14:44:21 -0500

    Leiming: openGL auto backup010218

--- 01_CreatingWindow/GenWimdow01.cpp
+++ 01_CreatingWindow/GenWimdow01.cpp
@@ -41,7 +41,11 @@ int main()
 
     while (!glfwWindowShouldClose(window))
     {
+        // Leiming: process input
         processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
